616cd590c63873b89fbde3f485f7fc6c3e4265e0
Askar Karasaev <askar.karasaev@gmail.com>
2019-02-26 21:20:18 +0900

    Sign in feature complete

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -9,5 +9,8 @@ RSpec.feature "Users can sign in" do
       click_button "Sign in"
 
        expect(page).to have_content "Signed in successfully."
+       expect(page).to have_content "Signed in as #{user.name}"
+       expect(subject).not_to have_content "Sign in"
+       expect(subject).not_to have_content "Sign up"
     end
 end 
